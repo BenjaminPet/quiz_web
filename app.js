40ea2e4b84567af9e2f48fa497f1376e1682b495
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2022-02-01 13:55:04 +0100

    d

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 function mekequiz() {
-    
+    var tag = document.createElement("div");
+    var text = document.createTextNode("<h2>question 1</h2> <p>hva bruker man python til</p> <button>Game developing</button><button>automasjon</button><button>endre utsene til websider</button>");
+    tag.appendChild(text);
+    var element = document.getElementById("new");
+    element.appendChild(tag);
+}
+function wrong() {
+    console.log("wrong");
+}
+function right() {
+    console.log("right");
 }
